8d887447ae60548105066b3003b4808ef9f7e62e
MaeganW <maeganwomble@gmail.com>
2017-08-30 10:07:46 -0500

    interfaces

---
+++ interfaces.js
@@ -0,0 +1,6 @@
+"use strict";
+// interface NamedPerson {
+//     firstname: string; //this property must exist where this interface is used
+//     age?: number; //this property is optional and may or may not be used
+//     [propName: string] //alternative property
+// } 
